b0e4edb870ed40ccaf606b1e23e7c7349ed2bc88
LukaMelinc <luka.melinc1@gmail.com>
2024-12-11 08:46:51 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+image_path = r"/Users/lukamelinc/Desktop/Belgija/Computer vision/WPO3/PSData/cat/LightProbe-1/Image_01.JPG"
+
+light_direction_path = r"/Users/lukamelinc/Desktop/Belgija/Computer vision/WPO3/PSData/cat/light_directions.txt"
+refined_light_path = r"/Users/lukamelinc/Desktop/Belgija/Computer vision/WPO3/PSData/cat/refined_light.txt"
